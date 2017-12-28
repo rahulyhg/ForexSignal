507edae628feb3e9e03c467672d1839198fbd7b7
TramPhan <tram.phan@mor.com.vn>
2017-12-28 10:49:53 +0700

    Update

--- sendAccountInfo.php
+++ sendAccountInfo.php
@@ -1,15 +1,20 @@
 <?PHP
-$number = $_REQUEST['number'];
-$broker = $_REQUEST['broker'];
-$name = $_REQUEST['name'];
-$balance = $_REQUEST['balance'];
-$profit = $_REQUEST['profit'];
-$accountInfo = [
-  'broker' => $broker,
-  'number' => $number,
-  'name' => $name,
-  'balance' => $balance,
-  'profit' => $profit,
-];
-file_put_contents("data/accountInfo/$number.txt", json_encode($accountInfo));
+$directory = 'data/accountInfo';
+if (isset($_REQUEST['number'])) {
+  $number = $_REQUEST['number'];
+  $broker = $_REQUEST['broker'];
+  $name = $_REQUEST['name'];
+  $balance = $_REQUEST['balance'];
+  $profit = $_REQUEST['profit'];
+  $accountInfo = [
+    'broker' => $broker,
+    'number' => $number,
+    'name' => $name,
+    'balance' => $balance,
+    'profit' => $profit,
+  ];
+  file_put_contents("$directory/$number.txt", json_encode($accountInfo));
+}
+$scannedDirectory = array_diff(scandir($directory), array('..', '.'));
+print json_encode($scannedDirectory);
 ?>
