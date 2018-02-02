247687de2e3e5131ad5657bd34111c96c0a23012
Tram Phan <tram.phan@mor.com.vn>
2018-02-02 10:37:08 +0700

    Update

--- private/api/getWhitelist.php
+++ private/api/getWhitelist.php
@@ -2,8 +2,8 @@
 require '../common/htmlDom.php';
 
 $html = file_get_html('https://icodrops.com/whitelist/');
-$result = [];
-$closed = [];
+$currentWhitelist = [];
+$closedWhitelist = [];
 foreach ($html->find('div.whtico-row') as $element) {
   @$tmp['name'] = $element->find("div.white_info h3", 0)->plaintext;
   @$tmp['type'] = $element->find("span.white-ico-category-name", 0)->plaintext;
@@ -11,15 +11,25 @@ foreach ($html->find('div.whtico-row') as $element) {
   @$tmp['status'] = $element->find("div.whitelist_date", 0)->plaintext;
   @$tmp['link'] = $element->find("div#white_join a", 0)->href;
   if (strpos($tmp['status'], "Closed") === false) {
-    $result[$tmp['name']] = $tmp;
+    $currentWhitelist[$tmp['name']] = $tmp;
   } else {
-    $closed[$tmp['name']] = $tmp;
+    $closedWhitelist[$tmp['name']] = $tmp;
   }
 }
-ksort($result);
-ksort($closed);
+ksort($currentWhitelist);
+ksort($closedWhitelist);
+$html = file_get_html('http://icowhitelists.com/files/telegram-tracker.html');
+$tmp = [];
+$telegramMember = [];
+foreach ($html->find('tbody tr') as $element) {
+  @$tmp['name'] = $element->find("td", 0)->plaintext;
+  @$tmp['member'] = $element->find("td", 2)->plaintext;
+  @$tmp['growth'] = $element->find("td", 3)->plaintext;
+  @$tmp['link'] = $element->find("td a", 0)->href;
+  $telegramMember[] = $tmp;
+}
 print "<table>";
-foreach ($result as $ico) {
+foreach ($currentWhitelist as $ico) {
   print "<tr>";
   print "<td>";
   print $ico['name'];
@@ -40,7 +50,7 @@ foreach ($result as $ico) {
 }
 print "</table>";
 print "<table>";
-foreach ($closed as $ico) {
+foreach ($closedWhitelist as $ico) {
   print "<tr>";
   print "<td>";
   print $ico['name'];
@@ -53,6 +63,25 @@ foreach ($closed as $ico) {
   print "</td>";
   print "<td>";
   print $ico['status'];
+  print "</td>";
+  print "<td>";
+  print $ico['link'];
+  print "</td>";
+  print "</tr>";
+}
+print "</table>";
+print "</table>";
+print "<table>";
+foreach ($telegramMember as $ico) {
+  print "<tr>";
+  print "<td>";
+  print $ico['name'];
+  print "</td>";
+  print "<td>";
+  print $ico['member'];
+  print "</td>";
+  print "<td>";
+  print $ico['growth'];
   print "</td>";
   print "<td>";
   print $ico['link'];
